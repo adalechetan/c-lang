5882de88340c676bafa6f71a1168fc46afa15dde
adalechetan <adalechetan454@gmail.com>
2024-07-27 19:51:30 -0700

    file

---
+++ .vscode/evenodd.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.>
+void main()
+{
+    int a,b;
+    printf("enter the frist number even ");
+scanf("%d" is frist num even);
+printf("enter the sconed number odd");
+scanf("%d" is sconed num odd);
+gatch();
+}
